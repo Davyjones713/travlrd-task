9a9f9e08b257fd3eedf04ddcaa7af5b5ed0fde7a
davyjones713 <datoiashvili713@gmail.com>
2024-08-08 12:45:34 +0400

    dynamic update

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import Tables from "./components/Tables";
 
 export const revalidate = 30;
+export const dynamic = "force-dynamic";
 
 export default function asyncHome() {
   return (
